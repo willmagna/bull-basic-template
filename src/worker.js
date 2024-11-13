55b72d73574f5f73e3afa7729c8d56fc325a3b41
William Magnabosco <williamlodea@gmail.com>
2024-11-13 10:45:17 -0400

    a

--- src/worker.js
+++ src/worker.js
@@ -1,8 +1,26 @@
 import "dotenv/config";
 import Queue from "./queue";
 
-Queue.process(async (job) => {
-  console.log(job.name);
-  console.log(job.data);
-  // throw new Error("asdfsdf");
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+Queue.process(async (job, done) => {
+  console.log("1/5");
+  job.progress(20);
+  await sleep(1000);
+  console.log("2/5");
+  job.progress(40);
+  await sleep(2000);
+  console.log("3/5");
+  job.progress(60);
+  await sleep(3000);
+  console.log("4/5");
+  job.progress(80);
+  await sleep(4000);
+  console.log("5/5");
+  job.progress(100);
+  await sleep(5000);
+
+  done();
 });
